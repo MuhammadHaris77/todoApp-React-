2abc34e3b32b6ee6e1dc977f245e0a2f7e0c5a95
muhammad-haris <harisqureshi415@gmail.com>
2024-10-02 21:26:57 -0700

    ui responsive

--- src/App.jsx
+++ src/App.jsx
@@ -31,14 +31,14 @@ function App() {
 
         <div class="input-group ">
           <input type="text" class="form-control" placeholder='Enter Your Title' onChange={(e) => setInput(e.target.value)} value={input} />
-          <span id="basic-addon2">  <button onClick={onSubmit} class="input-group-text btn btn-primary" >submit</button></span>
+          <span id="basic-addon2">  <button onClick={onSubmit} class="input-group-text btn btn-dark text-danger" >ADD TODO</button></span>
         </div>
 
 
-        <ul className="container p-5">
+        <ul className="container p-5  my-5 hadow-lg p-3 mb-5 bg-dark rounded">
           {
             todo.length > 0 ?
-              todo.map((item, index) => <li className=' text-center m-2 rounded p-2 bg-light'  style={{ width: "100%",fontWeight:"bold" }} key={index}> <span style={{paddingRight:"300px",paddingLeft:"400px"}}>{item}</span> <button className='btn btn-danger m-1' onClick={(e) => deleteTodo(index)}>Delete</button><button className='btn btn-success m-1' onClick={(e) => editTodo(index)}>Edit</button></li>) : <h1 className='text-center text-danger'>No Todo In List</h1>
+              todo.map((item, index) => <li className=' text-center m-2 rounded p-1 bg-light'  style={{ width: "100%",fontWeight:"bold" }} key={index}> <span style={{paddingRight:"25%",paddingLeft:"25%"}}>{item}</span> <button className='btn btn-danger m-1' onClick={(e) => deleteTodo(index)}>Delete</button><button className='btn btn-success m-1 ' onClick={(e) => editTodo(index)}> E d i t</button></li>) : <h1 className='text-center text-danger'>No Todo In List</h1>
           }
 
         </ul>
