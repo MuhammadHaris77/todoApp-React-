44970c8e18a72b0ecd5b470ffdb5e53d029e68ce
muhammad-haris <harisqureshi415@gmail.com>
2024-10-06 09:29:00 -0700

    prompt input value updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Card from './components/card';
 //import './App.css'
 
 function App() {
@@ -28,6 +29,17 @@ function App() {
     // setInput(todotemp[index], input)
   }
 
+  /// counter function 
+// const [counter,setCounter]= useState(0)
+// const addCounter=()=>{
+//   setCounter(counter +1)
+// }
+
+
+// const lessCounter=()=>{
+//    setCounter(counter -1)
+// }
+
 
   return (
     <div >
@@ -36,6 +48,7 @@ function App() {
         <div class="input-group ">
           <input type="text" class="form-control" placeholder='Enter Your Title' onChange={(e) => setInput(e.target.value)} value={input} />
           <span id="basic-addon2">  <button onClick={onSubmit} class="input-group-text btn btn-dark text-danger" >ADD TODO</button></span>
+        
         </div>
 
 
@@ -46,6 +59,27 @@ function App() {
           }
 
         </ul>
+     
+     {/* <div className="container p-5  my-5 hadow-lg p-3 mb-5 bg-light rounded text-center">
+     <h1 className='text-primary'>Counter  </h1>
+     {
+      counter > 5 ? "baray hogaye ho " : "chote ho abhi"
+     }
+     <h1 className='text-dark'>{counter}</h1>
+
+     <button  className='btn btn-success' onClick={addCounter}>add</button> <button className='btn btn-danger' onClick={lessCounter}>less</button>
+
+
+     </div> */}
+
+       
+{/* 
+        <Card title="hello world" description="this is my card"/>
+        <Card title="hello world" description="this is my card"/>
+        <Card title="hello world" description="this is my card"/> */}
+
+
+
 
 
 
---
+++ src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const Card=({title,description})=>{
+    
+    return(
+        <>
+        
+        <hr />
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <hr />    </>
+        
+    )
+}
+export default Card;
